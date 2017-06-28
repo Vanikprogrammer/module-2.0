2381c3a8fc7495264bd8a670809e7c3927a97f8b
Ivan Kutepov <ngk-trade@mail.ru>
2017-06-28 10:19:57 +0300

    вводим данные с клавиатуры

--- src/human/HumanParameters.java
+++ src/human/HumanParameters.java
@@ -1,7 +1,27 @@
 package human;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * Created by 1 on 28.06.2017.
  */
 public class HumanParameters {
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Как Вас зовут?");
+        String name = r.readLine();
+        System.out.println("В каком городе Вы живете?");
+        String city = r.readLine();
+        System.out.println("Сколько Вам лет? ");
+        int age = Integer.parseInt(r.readLine());
+        System.out.println("Какое Ваше хобби?");
+        String hobby = r.readLine();
+        
+
+
+
+    }
 }
