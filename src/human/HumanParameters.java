229b1064380dd0b5f306ac6d1863d559eb508cab
Ivan Kutepov <ngk-trade@mail.ru>
2017-06-28 10:33:31 +0300

    Вводим данные с клавиатуры

--- src/human/HumanParameters.java
+++ src/human/HumanParameters.java
@@ -19,16 +19,7 @@ public class HumanParameters {
         int age = Integer.parseInt(r.readLine());
         System.out.println("Какое Ваше хобби?");
         String hobby = r.readLine();
-        System.out.println("Вариант 1 (табличный):");
-        System.out.println();
-        System.out.println("Имя: " + name + "\n" + "Город: " + city + "\n" + "Возраст: " + age + "\n" + "Хобби: " + hobby);
-        System.out.println();
-        System.out.println("Вариант 2 (текстовый):");
-        System.out.println();
-        System.out.println("Человек по имени " + name +  " живет в городе " + city + "." + " Этому человеку " +  age + " лет и любит он заниматься " +  hobby + ".");
-        System.out.println();
-        System.out.println("Вариант 3 (иной):");
-        System.out.println(name + " - Имя" + "\n" + city + " - Город" + "\n" + age + " - Возраст" + "\n" + hobby + " - Хобби");
+        
 
 
 
